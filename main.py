c7c3b930e8bba3da1f5c967780960ceec224de32
Hassan Mehmood <hasanmehmood123@yahoo.com>
2020-04-07 16:05:55 +0500

    add and endpoint for file upload and save

--- main.py
+++ main.py
@@ -11,9 +11,31 @@ def read_root():
     return {"Hello": "World"}
 
 
-
 # --- Recieving id:int along with query params in variable q
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
+# --- File Upload and Saving 
+# https://github.com/tiangolo/fastapi/issues/426
+def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+
+
+# Using UploadFile
+@app.post("/uploadfile/")
+def create_upload_file(file: UploadFile = File(...)):
+    filepath = Path('uploaded_folder') / file.filename
+    save_upload_file(file, filepath)
+    return {"filename": file.filename}
+
+
+# Using bytes
+@app.post("/files/")
+def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
